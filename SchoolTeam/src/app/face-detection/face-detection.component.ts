8f25e7b16cd083d01543c4383b9e30009d1080f2
Deandra Rodricks <deandrarodricks@gmail.com>
2020-07-08 20:11:52 -0400

    'facedetection'

--- SchoolTeam/src/app/face-detection/face-detection.component.ts
+++ SchoolTeam/src/app/face-detection/face-detection.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-face-detection',
   templateUrl: './face-detection.component.html',
   styleUrls: ['./face-detection.component.scss']
 })
 export class FaceDetectionComponent implements OnInit {
+  forwardTimes = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    throw new Error("Method not implemented.");
   }
 
 }
